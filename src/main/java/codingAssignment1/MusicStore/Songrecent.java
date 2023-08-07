7a75aaac17241b82d93d7e73063898d3aaecf2fb
unknown <vinuachar121@gmail.com>
2023-08-07 09:30:07 +0530

    Java commit

---
+++ src/main/java/codingAssignment1/MusicStore/Songrecent.java
@@ -0,0 +1,69 @@
+package codingAssignment1.MusicStore;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Songrecent {
+
+	static int capacity = 4;
+
+	HashMap<String, Queue> map = new HashMap<String, Queue>();
+
+
+
+	public void adduser(String userid, String recent) {
+//
+		Songrecent sc = new Songrecent();
+		int flag;
+		Scanner input = new Scanner(System.in);
+
+		do {
+			System.out.println("Enter user name : ");
+			userid = input.next();
+			System.out.println("Enter song name : ");
+			recent = input.next();
+
+			sc.recentsong(recent, userid);
+
+			System.out.println("Do you want to add more songs : ");
+			flag = input.nextInt();
+
+		} while (flag != 0);
+		sc.recentsong(recent, userid);
+
+	}
+	
+	public void recentsong(String userid2, String recent) {
+
+		Queue<String> queue = map.get(userid2);
+		if (queue == null) {
+			queue = new LinkedList<>();
+			map.put(userid2, queue);
+		}
+
+		if (queue.size() >= capacity) {
+			queue.poll();
+		}
+
+		queue.add(recent);
+  
+		System.out.println("~~~~~~~");
+		System.out.println("Current playlist of user: " + map.get(userid2));
+		System.out.println("Current playlist of all users: " + map);
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Songrecent sc = new Songrecent();
+
+		sc.adduser("User1", "S1");
+
+	}
+
+}
---
+++ src/test/java/codingAssignment1/MusicStore/Storetest.java
@@ -0,0 +1,54 @@
+package codingAssignment1.MusicStore;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+
+public class Storetest {
+	
+	private static Songrecent songrecent;
+
+    @BeforeClass
+    public static void setUp() {
+        songrecent = new Songrecent();
+    }
+	 @Test
+	    public void testAddUserWithSongs() {
+	        // Add some users and their recent songs
+	        songrecent.recentsong("User1", "Song1");
+	        songrecent.recentsong("User2", "Song2");
+	        songrecent.recentsong("User1", "Song3");
+	        songrecent.recentsong("User2", "Song4");
+
+	        System.out.println();
+	        // Verify the playlists of specific users
+	        Assert.assertEquals(songrecent.map.get("User1").toString(), "[Song1, Song3]");
+	        Assert.assertEquals(songrecent.map.get("User2").toString(), "[Song2, Song4]");
+	    }
+
+	    
+	    @Test
+	    public void testAddMultipleSongsToUser() {
+	        // Add multiple songs to a single user to check capacity
+	        songrecent.recentsong("User4", "Song6");
+	        songrecent.recentsong("User4", "Song7");
+	        songrecent.recentsong("User4", "Song8");
+	        songrecent.recentsong("User4", "Song9");
+	        songrecent.recentsong("User4", "Song10");
+
+	        // Verify that the playlist has a maximum capacity of 4
+	        Assert.assertEquals(songrecent.map.get("User4").toString(), "[Song7, Song8, Song9, Song10]");
+	    }
+
+	    @Test
+	    public void testAddUserWithNoRecentSongs() {
+	        // Add a user without specifying any recent songs
+	        songrecent.recentsong("User5", "");
+
+	        // Verify that the playlist is empty for the user
+	        Assert.assertNull(songrecent.map.get("User5"));
+	    }
+	}
